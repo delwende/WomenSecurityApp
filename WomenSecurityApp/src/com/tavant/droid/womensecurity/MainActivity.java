af67d46e6db044469e8d94a5aba9ab7c6658d828
nasif <nasifn@gmail.com>
2013-05-21 11:22:03 +0530

    test commit

--- WomenSecurityApp/src/com/tavant/droid/womensecurity/MainActivity.java
+++ WomenSecurityApp/src/com/tavant/droid/womensecurity/MainActivity.java
@@ -7,8 +7,8 @@ import android.view.Menu;
 public class MainActivity extends Activity {
 
 	@Override
-	protected void onCreate(Bundle instance) {
-		super.onCreate(instance);
+	protected void onCreate(Bundle instance1) {
+		super.onCreate(instance1);
 		setContentView(R.layout.activity_main);
 	}
 
