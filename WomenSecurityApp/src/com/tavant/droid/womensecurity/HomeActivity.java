a67eed7795d3cb9ffc1ac00263adcc07f40c8f91
Rakesh Barik <rakesh.barik@tavant.com>
2013-05-23 17:33:57 +0530

    added access to phone contacts

--- WomenSecurityApp/src/com/tavant/droid/womensecurity/HomeActivity.java
+++ WomenSecurityApp/src/com/tavant/droid/womensecurity/HomeActivity.java
@@ -1,21 +1,51 @@
 package com.tavant.droid.womensecurity;
 
-import android.app.Activity;
+import com.tavant.droid.womensecurity.activities.FetchContactsActivity;
+
+import android.annotation.TargetApi;
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 
-import com.tavant.droid.womensecurity.sound.ScreamPlayer;
+public class HomeActivity extends FragmentActivity {
 
-public class HomeActivity extends Activity {
+	
 
+	@TargetApi(Build.VERSION_CODES.HONEYCOMB)
 	@Override
 	protected void onCreate(Bundle instance3) {
 		super.onCreate(instance3);
 		setContentView(R.layout.activity_main);
-//		ScreamPlayer soundPlayer = new ScreamPlayer(getApplicationContext());
-//		soundPlayer.setRepeatCount(2);
-//		soundPlayer.startRinging();
+		/*ScreamPlayer soundPlayer = new ScreamPlayer(getApplicationContext());
+		soundPlayer.setRepeatCount(2);
+		soundPlayer.startRinging();*/
 	}
 
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.home_menu, menu);
+		return true;
+	}
 
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
 
+		switch(item.getItemId()){
+		
+		case R.id.action_settings:
+			loadContacts();
+		
+		}
+		
+		return super.onOptionsItemSelected(item);
+	}
+
+	private void loadContacts() {
+		Intent intent = new Intent(this,FetchContactsActivity.class);
+		startActivity(intent);
+	}
 }
